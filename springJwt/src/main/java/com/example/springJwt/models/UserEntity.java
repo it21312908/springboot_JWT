eda59833facd4bd9f036f5c994211dd686df1d94
it21312908 <it21312908@my.sliit.lk>
2024-06-13 20:00:29 +0530

    UserEntity modified

--- springJwt/src/main/java/com/example/springJwt/models/UserEntity.java
+++ springJwt/src/main/java/com/example/springJwt/models/UserEntity.java
@@ -1,4 +1,20 @@
 package com.example.springJwt.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+
+    private String password;
 }
