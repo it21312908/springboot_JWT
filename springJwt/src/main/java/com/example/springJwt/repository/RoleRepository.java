eed6fd5486175936e1d7c612e38d551d6bd380cf
it21312908 <it21312908@my.sliit.lk>
2024-06-13 20:26:23 +0530

    RoleRepository added

---
+++ springJwt/src/main/java/com/example/springJwt/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.example.springJwt.repository;
+
+import com.example.springJwt.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
